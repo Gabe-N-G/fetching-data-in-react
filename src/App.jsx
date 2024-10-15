79f6746480d6120eb69da64fb0ee8a9785786971
g g <gabedotgutierrez@gmail.com>
2024-10-15 15:00:54 -0400

    linked weathersevice connected to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import * as weatherService from './services/weatherService.js'
+//alternatively  
+//import {show} from './services/weatherService.js'
+
 
 const App = () => {
+  const fetchData = async () => {
+    const data = await weatherService.show('New York');
+    console.log('Data:', data);
+  };
 
   return (
-    <h1>Hello world!</h1>
+    <main>
+      <h1>Weather API</h1>
+      <button onClick={fetchData}>Fetch Weather Data</button>
+    </main>
   );
-}
+};
 
-export default App
-
-
+export default App;
