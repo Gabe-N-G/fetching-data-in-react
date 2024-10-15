d8e84f0ae33d9a2b9aa2572b5cddd78c1cfc2b45
g g <gabedotgutierrez@gmail.com>
2024-10-15 15:25:07 -0400

    passed weathersearch, weathersearch working, no visible output

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import * as weatherService from './services/weatherService.js'
 //alternatively  
 //import {show} from './services/weatherService.js'
+import WeatherSearch from './components/WeatherSearch.jsx'
 
 
 const App = () => {
-  const fetchData = async () => {
-    const data = await weatherService.show('New York');
-    console.log('Data:', data);
+  const [weather,setWeather] = useState({})
+
+  const fetchData = async (city) => {
+    const data = await weatherService.show(city);
+    const newWeatherState = {
+      location: data.location.name,
+      temperature: data.current.temp_f,
+      condition: data.current.condition.text,
+    };
+    setWeather(newWeatherState);
   };
+  // The following log should be outside of the fetchData function
+  console.log('State:', weather);
 
   return (
     <main>
       <h1>Weather API</h1>
-      <button onClick={fetchData}>Fetch Weather Data</button>
+      <WeatherSearch fetchData={fetchData}/>
+      {/* <button onClick={fetchData}>Fetch Weather Data</button> */}
     </main>
   );
 };
